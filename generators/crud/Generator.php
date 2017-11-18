eb44d0f1fb095fce28b7ddb70b6f8e6e65d827f5
prodex <iprodex@mail.ru>
2017-11-18 14:54:10 +0300

    Exclude columns from form.

--- generators/crud/Generator.php
+++ generators/crud/Generator.php
@@ -2,12 +2,28 @@
 
 namespace prodex\yii\gii\generators\crud;
 
+use \yii\gii\generators\crud\Generator;
+
 /**
  * Generates CRUD
  *
- * @see \yii\gii\generators\crud\Generator
+ * @see Generator
  */
-class Generator extends \yii\gii\generators\crud\Generator
+class Generator extends Generator
 {
-    
+    /**
+     * @var string[]
+     */
+    public $excludeColumnsFromForm = ['created_at', 'create_time', 'updated_at', 'create_time'];
+
+    /**
+     * @inheritdoc
+     */
+    public function getColumnNames()
+    {
+        $columnNames = parent::getColumnNames();
+        $columnNames = array_diff($columnNames, $this->excludeColumnsFromForm);
+
+        return $columnNames;
+    }
 }
